441353a7d7427d1a099aea80cff84a16528be5f3
dimon2292 <115736998+dimon2292@users.noreply.github.com>
2022-12-24 00:35:18 +0300

    lab3

---
+++ task1_2.py
@@ -0,0 +1,16 @@
+OUTPUT_FILE = "output.txt"
+
+
+def task():
+    with open(OUTPUT_FILE, 'w') as f:
+        for i in range(1, 11):
+            sequence = '*' * (i)
+            f.write(f'{sequence:>10}\n')# TODO записать лесенку в файл
+
+
+if __name__ == "__main__":
+    task()
+
+    with open(OUTPUT_FILE) as file:
+        for line in file:
+            print(line, end="")
